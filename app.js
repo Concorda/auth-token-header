60e4ac1e29cf431b3037e02f123924f440977542
Mircea Alexandru <mircea.alexandru@gmail.com>
2015-07-16 16:49:15 +0300

    added tokenkey default option

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 "use strict";
+var _ = require('lodash')
 
 module.exports = function ( options ) {
   var seneca = this
   var plugin = 'seneca-auth-token-plugin'
+  options = _.extend({}, default_options, options)
 
   function setToken(args, cb){
-    var tokenkey  = args.tokenkey
+    var tokenkey  = args.tokenkey || options.tokenkey
     var token     = args.token
     var res       = args.res
 
@@ -15,7 +17,7 @@ module.exports = function ( options ) {
   }
 
   function getToken(args, cb){
-    var tokenkey  = args.tokenkey
+    var tokenkey  = args.tokenkey || options.tokenkey
     var req       = args.req
     cb(null, {token: req.get( tokenkey )})
   }
---
+++ default-options.js
@@ -0,0 +1,3 @@
+module.exports = {
+  "tokenkey": "X-Auth-Token"
+}

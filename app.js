eb16d6efd834c1e2cdaf0413b5a36392ef301316
mirceaalexandru <mircea.alexandru@gmail.com>
2015-08-25 14:09:24 +0300

    change to module

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
 "use strict";
-var _ = require('lodash')
-var default_options = require('./default-options.js')
+
+var _ = require( 'lodash' )
+var default_options = require( './default-options.js' )
 
 module.exports = function ( options ) {
-  var seneca = this
-  var plugin = 'seneca-auth-token-plugin'
-  options = _.extend({}, default_options, options)
 
-  function setToken(args, cb){
-    var tokenkey  = args.tokenkey || options.tokenkey
-    var token     = args.token
-    var res       = this.fixedargs.res$
+  var seneca = this
+
+  options = _.extend( {}, default_options, options || {} )
+
+  function setToken( args, cb ) {
+    var tokenkey = args.tokenkey || options.tokenkey
+    var token = args.token
+    var res = this.fixedargs.res$
 
     res.set( tokenkey, token )
 
-    cb(null, {token: token})
+    cb( null, {token: token} )
   }
 
-  function getToken(args, cb){
-    var tokenkey  = args.tokenkey || options.tokenkey
-    var req       = this.fixedargs.req$
-    cb(null, {token: req.get( tokenkey )})
+  function getToken( args, cb ) {
+    var tokenkey = args.tokenkey || options.tokenkey
+    var req = this.fixedargs.req$
+    cb( null, {token: req.get( tokenkey )} )
   }
 
-  seneca.add({role: 'auth', set: 'token'}, setToken)
-  seneca.add({role: 'auth', get: 'token'}, getToken)
+  seneca.add( {role: 'auth', set: 'token'}, setToken )
+  seneca.add( {role: 'auth', get: 'token'}, getToken )
 
-  return {
-    name:plugin
-  }
 }
